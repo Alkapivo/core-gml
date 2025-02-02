36112a2f7b45c83b9591f86fc9f55cc7b0d2e0ca
Alkapivo <alkapivo@gmail.com>
2025-02-02 03:50:14 +0100

    Add stitch.sh

---
+++ stitch.sh
@@ -0,0 +1,97 @@
+#!/bin/bash -e -x
+
+###param {string} name
+###param {?string} [msg]
+function check_var() {
+  local var_name="$1"
+  local var_msg="$2"
+  local var_value="${!var_name-}" 
+
+  if [[ -z "$var_value" || "$var_value" == "0" ]]; then
+    echo "[stitch.sh] ERROR: \$$var_name is not setup. Hint: $var_msg"
+  fi
+}
+
+function check_env() {
+  check_var "STITCH_IGOR" "Path to Igor.exe"
+  check_var "STITCH_PROJECT_NAME" "Name of yyp file without extension"
+  check_var "STITCH_PROJECT_PATH" "Path to yyp project without / at the end"
+  check_var "STITCH_USER_PATH" "Path to AppData/Roaming/GameMakerStudio2/{USER} without / at the end"
+  check_var "STITCH_RUNTIME_PATH" "Path to GameMakerStudio2/Cache/runtimes/{RUNTIME} without / at the end"
+  check_var "STITCH_RUNTIME" "Available options: VM, YYC"
+  check_var "STITCH_PLATFORM" "Available options: windows"
+}
+
+function build_and_run {
+  check_env
+  $STITCH_IGOR \
+    --project=$STITCH_PROJECT_PATH/$STITCH_PROJECT_NAME.yyp \
+    --user=$STITCH_USER_PATH \
+    --runtimePath=$STITCH_RUNTIME_PATH \
+    --runtime=$STITCH_RUNTIME \
+    --cache=$STITCH_PROJECT_PATH/tmp/igor/cache \
+    --temp=$STITCH_PROJECT_PATH/tmp/igor/temp \
+    --of=$STITCH_PROJECT_PATH/tmp/igor/temp \
+    --tf=$STITCH_PROJECT_PATH/$STITCH_PROJECT_NAME.zip \
+    -- $STITCH_PLATFORM Run
+}
+
+function build_and_run_vm {
+  check_env
+  $STITCH_IGOR \
+    --project=$STITCH_PROJECT_PATH/$STITCH_PROJECT_NAME.yyp \
+    --user=$STITCH_USER_PATH \
+    --runtimePath=$STITCH_RUNTIME_PATH \
+    --runtime=VM \
+    --cache=$STITCH_PROJECT_PATH/tmp/igor/cache \
+    --temp=$STITCH_PROJECT_PATH/tmp/igor/temp \
+    --of=$STITCH_PROJECT_PATH/tmp/igor/temp \
+    --tf=$STITCH_PROJECT_PATH/$STITCH_PROJECT_NAME.zip \
+    -- $STITCH_PLATFORM Run
+}
+
+function build_and_run_yyc {
+  check_env
+  $STITCH_IGOR \
+    --project=$STITCH_PROJECT_PATH/$STITCH_PROJECT_NAME.yyp \
+    --user=$STITCH_USER_PATH \
+    --runtimePath=$STITCH_RUNTIME_PATH \
+    --runtime=YYC \
+    --cache=$STITCH_PROJECT_PATH/tmp/igor/cache \
+    --temp=$STITCH_PROJECT_PATH/tmp/igor/temp \
+    --of=$STITCH_PROJECT_PATH/tmp/igor/temp \
+    --tf=$STITCH_PROJECT_PATH/$STITCH_PROJECT_NAME.zip \
+    -- $STITCH_PLATFORM Run
+}
+
+function clean_yyp {
+  check_envcheck_env
+  echo "[clean_yyp] Start clean tmp at $STITCH_PROJECT_PATH/tmp/igor"
+  rm -rf $STITCH_PROJECT_PATH/tmp/igor
+  echo "[clean_yyp] Cleaned"
+}
+
+function sync_yyp {
+  echo "[stitch.sh] Start gm-cli watch"
+  gm-cli watch
+}
+
+# init
+if [ -f "stitch.env.sh" ]; then
+    echo "[stitch.sh] source stitch.env.sh"
+    source stitch.env.sh
+else
+  echo "[stitch.sh] stitch.env.sh was not found"
+fi
+
+echo "[stitch.sh] Run check_env"
+check_env
+
+# print help
+echo "[stitch.sh] Source this file to load commands:"
+echo "  build_and_run       Build usign \$STITCH_RUNTIME: $STITCH_RUNTIME"
+echo "  build_and_run_vm    Build using Virtual Machine and run it"
+echo "  build_and_run_yyc   Build using YoYo Compiler and run it"
+echo "  clean_yyp           Remove tmp/igor folder created by CLI builder"
+echo "  sync_yyp            Runs gm-cli watch"
+echo ""
